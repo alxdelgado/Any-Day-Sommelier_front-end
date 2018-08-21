258624266b8e0b057e029e8d0bd09878375e31d3
Alex Delgado <delgadoalex@icloud.com>
2018-08-21 16:42:11 -0500

    Home Container Modified

--- anyday-sommelier-app/src/HomeContainer/index.js
+++ anyday-sommelier-app/src/HomeContainer/index.js
@@ -9,18 +9,58 @@ class HomeContainer extends React.Component {
       meat: []
     }
   }
+  getWines = async () => {
+    try {
+      const wineData = await fetch ("http://localhost:8000/api/wines");
+      const wines = wineData.json();
+      return wines;
+    } catch(err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  componentDidMount() {
+    this.getWines().then((res) => {
+      this.setState({
+        wines: res.wineData
+      });
+    });
+  }
+
+  addWines = async (wines, event) => {
+    event.preventDefault();
+    try {
+      const createdWines = await fetch("http://localhost:8000/api/wines", {
+        method: "POST",
+        body: JSON.stringify(wines),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      const parsedResponse = await createdWines.json();
+      console.log(wines);
+      this.state = wines;
+      console.log(wines);
+      this.props.history.push("/wines/");
+
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   render() {
     return(
       <div>
         <img className="placeholderName" src="#" alt="Logo"></img>
         <img className="Hero" src="/Images/TypeLogo.svg"></img>
 
-        <img className="placeholderName" src="#" alt="Example Pairing"></img>
-        <img className="placeholderName" src="#" alt="Example Pairing"></img>
-        <img className="placeholderName" src="#" alt="Example Pairing"></img>
         <p>Welcome to Anyday Sommelier- the handy app for discovering your new favorite wine and entre pairings. Tap the icons to get started or sign in below to create an account, allowing to save, vote, and favorite pairings.</p>
 
         <input type='text' placeholder='wine' value='wine' />
+
+        {this.state.wine}
 
       </div>
     )
