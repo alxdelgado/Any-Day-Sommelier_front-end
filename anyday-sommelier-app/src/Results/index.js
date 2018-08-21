2bc180fd0bbf815e9647391107dfbb43dd9faf64
David Kolia <Dkolia@icloud.com>
2018-08-21 10:27:13 -0500

    Added basic selection pulldowns

--- anyday-sommelier-app/src/Results/index.js
+++ anyday-sommelier-app/src/Results/index.js
@@ -5,8 +5,20 @@ const Results = () => {
   return (
     <div>
       <img src="Images/SelectFood.svg"></img>
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
       <img src="Images/Or.svg"></img>
       <img src="Images/SelectWine.svg"></img>
+        <select>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
     </div>
     )
 };
